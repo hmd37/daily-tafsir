9d64661f44858bd5b090fa3e64143b1afae60f67
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2025-11-10 09:53:20 +0400

    initial

---
+++ daily_tafsir/main.py
@@ -0,0 +1,71 @@
+import os
+
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from telegram import Bot
+
+load_dotenv()
+
+app = FastAPI(title="Daily Ayat API")
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+
+def scrape_daily_ayat_and_tafsir():
+    daily_ayat_url = "https://www.kuranvemeali.com/"
+    response = requests.get(daily_ayat_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    ayat_meal_link = (
+        daily_ayat_url + soup.find("a", {"class": "btn-sm btn-warning"})["href"]
+    )
+    ayat_tafsir_link = ayat_meal_link.replace("-meali", "-tefsiri")
+
+    ayat_div = soup.find("div", {"class": "col-sm-12 team-block text-center"})
+    ayat = ayat_div.find("p").get_text(strip=True)
+
+    tafsir_soup = BeautifulSoup(requests.get(ayat_tafsir_link).text, "html.parser")
+    tafsir_div = tafsir_soup.find_all("div", {"class": "tefsir"})[-1]
+
+    for tag in tafsir_div.find_all(["h4", "a", "h5"]):
+        tag.decompose()
+
+    tafsir = tafsir_div.get_text(strip=True)
+    tafsir = tafsir.replace("\r", "").replace("\n", " ")
+
+    return {
+        "ayat": ayat,
+        "tafsir": tafsir,
+        "tafsir_link": ayat_tafsir_link,
+    }
+
+
+@app.get("/")
+async def send_daily_tafsir():
+    """Scrape ayet & tefsir and send to Telegram group."""
+    data = scrape_daily_ayat_and_tafsir()
+
+    msg = (
+        "<b>📖 Günün Ayeti</b>\n\n"
+        f"{data['ayat']}\n\n"
+        "━━━━━━━━━━━━━━━\n"
+        "<b>📘 Tefsir</b>\n\n"
+        f"{data['tafsir']}\n\n"
+        f"🕊 <i>Kaynak: {data['tafsir_link']}</i>"
+    )
+
+    await bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text=msg,
+        parse_mode="HTML",
+    )
+
+    return {
+        "status": "sent",
+        "data": data,
+    }
