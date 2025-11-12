d35ffa7c39f55a6bc286d896cf6a728735e646c0
Ahmad Allahverdiyev <dvlpr37@gmail.com>
2025-11-12 23:04:09 +0400

    Fix env

--- daily_tafsir/main.py
+++ daily_tafsir/main.py
@@ -9,8 +9,8 @@ from telegram import Bot
 load_dotenv()
 
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN").strip()
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID").strip()
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
